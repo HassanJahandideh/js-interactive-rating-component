e44f4ded489ccd47b48c336d16514ad148743da3
Hassan Jahandideh <jahandideh.2083@gmail.com>
2025-03-07 16:53:43 +0300

    ...

---
+++ index.js
@@ -0,0 +1,8 @@
+const btnSubmit = document.querySelector(".btn-submit");
+const thankYou = document.querySelector(".end-container");
+const mainContainer = document.querySelector(".main-container");
+const btnRates = document.querySelectorAll(".btn-rate");
+const rating = document.querySelector(".rating");
+
+
+
