3a23601b868baeb423dff2490283ffcb3a8e5534
Hassan Jahandideh <jahandideh.2083@gmail.com>
2025-03-16 16:30:17 +0300

    making buttons and submit buttons interactive

--- index.js
+++ index.js
@@ -4,5 +4,20 @@ const mainContainer = document.querySelector(".main-container");
 const btnRates = document.querySelectorAll(".btn-rate");
 const rating = document.querySelector(".rating");
 
+let selectedRate = null;
 
+btnRates.forEach((btn) => {
+   btn.addEventListener('click', () => {
+      selectedRate = btn.textContent;
+   });
+});
 
+btnSubmit.addEventListener('click', () => {
+   if (!selectedRate) {
+      alert("Please select a rating");
+      return;
+   }
+   mainContainer.style.display = "none";
+   thankYou.style.display = "block";
+   rating.textContent = selectedRate;
+})
